7c1008c90f2f22bcbc7aa5a17aad5b83e3039747
Eldar Gyulmagomedov <steal.vip@gmail.com>
2023-02-18 12:31:05 +0300

    Написал первую задачу по нахождению второй цифры в трехзначном числе

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+ Console.Write("Введите число от 100 до 999: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ if (number < 999 && number > 100)
+ {
+    int secondValue = number % 100 / 10;
+    Console.WriteLine("Вторая цифра числа "+ number + " равна " + secondValue);
+ }
+else
+{
+    Console.WriteLine("Число " + number + " не является трехзначным");
+}
